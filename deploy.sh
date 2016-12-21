a25fef0863884250221e8b91da912f762224be88
sylxjtu <a443833359@gmail.com>
2016-12-21 15:05:05 +0800

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -50,13 +50,9 @@ git add .
 git commit -m "Deploy to Frontend: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-chmod 600 ../key/deploy_key
+chmod 600 ../deploy_key
 eval `ssh-agent -s`
-ssh-add ../key/deploy_key
+ssh-add ../deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
