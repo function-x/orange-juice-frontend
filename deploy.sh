75ef3724e340051dc6a60c9ba9ecb568a2eae7fd
sylxjtu <a443833359@gmail.com>
2016-12-21 18:35:43 +0800

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -46,7 +46,7 @@ fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add .
+git add -A
 git commit -m "Deploy to Frontend: ${SHA}"
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
