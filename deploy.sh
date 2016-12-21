3a7d961b42123e7c0effe11d8f77c45d24c5fd10
sylxjtu <a443833359@gmail.com>
2016-12-21 15:02:38 +0800

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -54,7 +54,6 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../key/deploy_key.enc -out ../key/deploy_key -d
 chmod 600 ../key/deploy_key
 eval `ssh-agent -s`
 ssh-add ../key/deploy_key
